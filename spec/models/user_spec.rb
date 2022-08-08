1725a2cacb85eaff66ea9410f7c06abe8d5c5839
sho55555 <shogo61019@gmail.com>
2022-08-08 11:06:17 +0900

    テストコードで確認する動作の洗い出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー新規登録" do
+    it "ニックネームが空だと登録できない" do
+    end
+
+    it "メールアドレスが空だと登録できない" do
+    end
+
+    it "既に登録されているメールアドレスは登録できない" do
+    end
+
+    it "@を含まないメールアドレスは登録できない" do
+    end
+
+    it "passwordが空だと登録できない" do
+    end
+
+    it "passwordが6文字以上でないと登録できない" do
+    end
+
+    it "passwordが半角英数字混合でないと登録できない" do
+    end
+
+    it "passwordと確認用passwordが一致していないと登録できない" do
+    end
+
+    it "名字が空だと登録できない" do
+    end
+
+    it "名前が空だと登録できない" do
+    end
+
+    it "名字(カナ)が空だと登録できない" do
+    end
+
+    it "名前(カナ)が空だと登録できない" do
+    end
+
+    it "生年月日が空だと登録できない" do
+    end
+  end
 end
