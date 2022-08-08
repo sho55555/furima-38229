42607533cb203593d98179b8fb5527af3b9a65cb
sho55555 <shogo61019@gmail.com>
2022-08-08 18:23:00 +0900

    モデルを正規表記に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥々]/ }
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
