f74d2993c3006f10cc583d913287a4a367fc2906
sho55555 <shogo61019@gmail.com>
2022-08-12 19:51:40 +0900

    出品が正しく登録できるための修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  protect_from_forgery
   
   def index
+    @items = Item.all
   end
 
   def new
@@ -9,7 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Prefecture < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '北海道' },
@@ -29,7 +29,7 @@ class Genre < ActiveHash::Base
     { id: 27, name: '京都府' },
     { id: 28, name: '大阪府' },
     { id: 29, name: '兵庫県' },
-    { id: 30, name: '奈良県' }
+    { id: 30, name: '奈良県' },
     { id: 31, name: '和歌山県' },
     { id: 32, name: '鳥取県' },
     { id: 33, name: '島根県' },
