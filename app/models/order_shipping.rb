0c80f20f037fc29306d0921a0f01349d74103241
sho55555 <shogo61019@gmail.com>
2022-08-18 19:06:37 +0900

    バリデーション（with_options)

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -2,16 +2,17 @@ class OrderShipping
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code,         presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city,                presence: true
-  validates :addresses,           presence: true
-  validates :phone_number,        presence: true, format: { with: /\A\d{10,11}\z/ }
+  with_options presence: true
+  validates :postal_code,         format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank" }
+  validates :city
+  validates :addresses
+  validates :phone_number,        format: { with: /\A\d{10,11}\z/ }
 
-  validates :user_id,             presence: true
-  validates :item_id,             presence: true
+  validates :user_id
+  validates :item_id
 
-  validates :token,               presence: true
+  validates :token
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
